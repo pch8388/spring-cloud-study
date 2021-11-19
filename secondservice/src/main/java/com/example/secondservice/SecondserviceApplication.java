952b2828a4b9df60d67fe8fc7571cb624eb40b44
SC <pch838811@gmail.com>
2021-11-19 23:42:29 +0900

    service init

---
+++ secondservice/src/main/java/com/example/secondservice/SecondserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondserviceApplication.class, args);
+	}
+
+}
