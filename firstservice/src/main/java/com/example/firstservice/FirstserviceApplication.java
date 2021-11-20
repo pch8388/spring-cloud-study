0c97835f1f28c01db366ed85d74698e04fbdbdbc
SC <pch838811@gmail.com>
2021-11-20 21:37:04 +0900

    spring cloud zuul

--- firstservice/src/main/java/com/example/firstservice/FirstserviceApplication.java
+++ firstservice/src/main/java/com/example/firstservice/FirstserviceApplication.java
@@ -2,12 +2,19 @@ package com.example.firstservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstserviceApplication.class, args);
 	}
 
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to the First service";
+	}
 }
---
+++ zuul-service/src/test/java/me/study/zuulservice/ZuulServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package me.study.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
