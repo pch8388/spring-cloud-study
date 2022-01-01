ed034812761c824b13984f755c42a859f200f0f4
SC <pch838811@gmail.com>
2022-01-01 21:16:12 +0900

    spring cloud config server - file read

---
+++ spring-config/src/test/java/me/study/springconfig/SpringConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.springconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
