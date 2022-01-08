ea39caa8bd0cedf9dc949b14cf0823ee95c1feb0
SC <pch838811@gmail.com>
2022-01-08 14:16:40 +0900

    spring cloud config refresh test

--- secondservice/src/main/java/com/example/secondservice/SecondController.java
+++ secondservice/src/main/java/com/example/secondservice/SecondController.java
@@ -1,16 +1,15 @@
-package com.example.firstservice;
+package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 @RefreshScope
-public class FirstController {
+public class SecondController {
 
     @Value("${common.property}")
     private String property;
@@ -22,13 +21,6 @@ public class FirstController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome to the First service";
-    }
-
-    @GetMapping("/hello/{id}")
-    public String hello(@PathVariable String id) throws InterruptedException {
-        log.info("First service welcome start !!");
-        Thread.sleep(5000);
-        return "hello " + id;
+        return "Welcome to the Second service";
     }
 }
--- secondservice/src/main/java/com/example/secondservice/SecondserviceApplication.java
+++ secondservice/src/main/java/com/example/secondservice/SecondserviceApplication.java
@@ -6,16 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SecondserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SecondserviceApplication.class, args);
 	}
-
-	@GetMapping("/welcome")
-	public String welcome() {
-		return "Welcome to the Second service";
-	}
-
 }
