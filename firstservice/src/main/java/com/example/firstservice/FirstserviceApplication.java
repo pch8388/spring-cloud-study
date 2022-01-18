57e65d1d7ce1ee21a53e33b06f34c97fa0641ccf
SC <pch838811@gmail.com>
2022-01-18 20:35:23 +0900

    feign client logging

--- firstservice/src/main/java/com/example/firstservice/FirstserviceApplication.java
+++ firstservice/src/main/java/com/example/firstservice/FirstserviceApplication.java
@@ -1,13 +1,20 @@
 package com.example.firstservice;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 public class FirstserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstserviceApplication.class, args);
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
 	}
 }
