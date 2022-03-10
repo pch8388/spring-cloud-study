0c193f9fbafce1625aadca47959037fb82bbbe75
sc <pch838811@gmail.com>
2022-03-10 21:34:13 +0900

    security 사용

--- firstservice/src/main/java/com/example/firstservice/FirstserviceApplication.java
+++ firstservice/src/main/java/com/example/firstservice/FirstserviceApplication.java
@@ -6,12 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
-@EnableResourceServer
 public class FirstserviceApplication {
 
 	public static void main(String[] args) {
---
+++ firstservice/src/main/java/com/example/firstservice/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.firstservice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+            .anyRequest().authenticated()
+            .and()
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
